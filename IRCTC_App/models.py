059e73095e6e30bbfe8b19cd42584a2492023e52
praajj <parteprajaktaa@gmail.com>
2025-01-16 23:57:31 +0530

    Added user model

--- IRCTC_App/models.py
+++ IRCTC_App/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=256)
+    role = models.CharField(max_length=10, choices=[('admin', 'Admin'), ('user', 'User')], default='user')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'User'  # Use the existing table name
+        managed = False  # Do not manage this table with migrations
